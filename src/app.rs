b3e256e3edbe50de6e8acc6a40f6fc139be45575
Alistair Keiller <alistair@keiller.net>
2023-09-26 15:19:06 -0700

    add plot

--- src/app.rs
+++ src/app.rs
@@ -1,33 +1,30 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+use egui::plot::{Legend, Line, Plot, PlotBounds, PlotPoints, PlotResponse};
 
-    // this how you opt-out of serialization of a member
-    #[serde(skip)]
-    value: f32,
+const MINV: f64 = 0.;
+const MAXV: f64 = 1.;
+const MINP: f64 = 0.;
+const MAXP: f64 = 1.;
+const LINERES: i64 = 1000;
+const GAMMA: f64 = 5. / 3.;
+
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)]
+pub struct TemplateApp {
+    pressure: f64,
+    volume: f64,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            pressure: 0.5,
+            volume: 0.5,
         }
     }
 }
 
 impl TemplateApp {
-    /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
@@ -37,80 +34,75 @@ impl TemplateApp {
 }
 
 impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
-
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
-        #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        _frame.close();
-                    }
-                });
-            });
-        });
-
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
-                });
-            });
-        });
+        let Self { pressure, volume } = self;
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
+            let PlotResponse {
+                response,
+                inner: pointer_coordinate,
+                ..
+            } = Plot::new("my_plot")
+                .legend(Legend::default())
+                .height(500.)
+                .allow_zoom(false)
+                .allow_scroll(false)
+                .allow_double_click_reset(false)
+                .allow_boxed_zoom(false)
+                .allow_drag(false)
+                .show_x(false)
+                .show_y(false)
+                .show_axes([false, false])
+                .show(ui, |plot_ui| {
+                    plot_ui.set_plot_bounds(PlotBounds::from_min_max([MINV, MINP], [MAXV, MAXP]));
+                    plot_ui.line(
+                        Line::new(
+                            (0..LINERES)
+                                .map(|i| {
+                                    let v = MINV + i as f64 * (MAXV - MINV) / LINERES as f64;
+                                    [v, *pressure * *volume / v]
+                                })
+                                .collect::<PlotPoints>(),
+                        )
+                        .name("isothermic"),
+                    );
+                    plot_ui.line(
+                        Line::new(
+                            (0..LINERES)
+                                .map(|i| {
+                                    let v = MINV + i as f64 * (MAXV - MINV) / LINERES as f64;
+                                    [v, *pressure * (*volume).powf(GAMMA) / v.powf(GAMMA)]
+                                })
+                                .collect::<PlotPoints>(),
+                        )
+                        .name("adiabatic"),
+                    );
+                    plot_ui.line(
+                        Line::new(PlotPoints::new(Vec::from([
+                            [MINV, *pressure],
+                            [MAXV, *pressure],
+                        ])))
+                        .name("isobaric"),
+                    );
+                    plot_ui.line(
+                        Line::new(PlotPoints::new(Vec::from([
+                            [*volume, MINP],
+                            [*volume, MAXP],
+                        ])))
+                        .name("isochoric"),
+                    );
+                    plot_ui.pointer_coordinate()
+                });
+            if response.dragged() && response.hovered() {
+                if let Some(pointer_coordinate) = pointer_coordinate {
+                    *volume = pointer_coordinate.x;
+                    *pressure = pointer_coordinate.y;
+                }
+            }
         });
-
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally choose either panels OR windows.");
-            });
-        }
     }
 }
